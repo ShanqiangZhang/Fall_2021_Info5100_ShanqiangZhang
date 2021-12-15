45e7da687be5110bb67e339d23c917471f5dcc63
ShanqiangZhang <zhang.shanq@northeastern.edu>
2021-12-14 22:37:20 -0800

    final

--- Final/src/Q2/Q2.java
+++ Final/src/Q2/Q2.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Q2 {
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public static List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> hash = new HashMap<>();
         for (int i = 0; i < strs.length; i++) {
             char[] s_arr = strs[i].toCharArray();
@@ -22,6 +22,17 @@ public class Q2 {
 
         }
         return new ArrayList<List<String>>(hash.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs1 = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs1));
+        String[] strs2 = {""};
+        System.out.println(groupAnagrams(strs2));
+        String[] strs3 = {"a"};
+        System.out.println(groupAnagrams(strs3));
+
+
     }
 
 }
--- Final/src/Q3/Q3.java
+++ Final/src/Q3/Q3.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Q3 {
-    public int subarraySum(int[] nums, int k) {
+    public static int subarraySum(int[] nums, int k) {
         Map<Integer, Integer> count = new HashMap<>();
         int total = 0;
         int res = 0;
@@ -19,5 +19,12 @@ public class Q3 {
             count.put(total, count.getOrDefault(total, 0) + 1);
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,1,1};
+        System.out.println(subarraySum(nums1, 2));
+        int[] nums2 = {1,2,3};
+        System.out.println(subarraySum(nums2, 3));
     }
 }
